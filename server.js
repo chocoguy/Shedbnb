f80040e3d96329755b357f1bc2740e1b64b87c3f
Chocoguy <e.zarco7@gmail.com>
2020-06-21 18:14:47 -0500

    added cors/fixed bugs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const connectDB = require('./config/db.js')
-
+const cors = require('cors')
 const app = express();
 require('dotenv').config()
-
+const path = require('path')
+app.use(cors())
 //* DB connection
 connectDB();
 
@@ -11,19 +12,7 @@ connectDB();
 app.use(express.json({extended: false}))
 
 
-//code below used for deployment, comment for dev.. uncommment for production
 
-
-
-app.use(express.static('client/build'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-})
-
-
-
-//End production code
 
 
 //? Routes
@@ -32,6 +21,14 @@ app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/auth', require('./routes/api/auth'))
 
+
+//Code below used for deployment 
+
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server up and running  ${PORT}`))
